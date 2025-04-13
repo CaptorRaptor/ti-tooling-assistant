b410521796f5625a5d2d24043096706369e383e2
CaptorRaptor <98981216+CaptorRaptor@users.noreply.github.com>
2025-04-13 03:52:33 +0200

    fixing images

--- app/ui/ti-logo.tsx
+++ app/ui/ti-logo.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import config from '@/next.config'
 
 const title = 'TI: LEGACY TOOL ASSISTANT';
 
 export default function TILogo() {
     return (
         <div className='flex flex-row items-center leading-none space-x-4'>
-            <Image src='/TI-192x192.png' width={192} height={192} alt='Logo' className='h-12 w-12'/>
+            <Image src={`${config.basePath}/TI-192x192.png`} width={192} height={192} alt='Logo' className='h-12 w-12'/>
             <Link
                 key={'logo-link'}
                 href={'/'}
--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  output: 'export'
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    output: 'export',
+    basePath: isProd? '/ti-tooling-assistant' : '',
 }
 
 export default nextConfig;
